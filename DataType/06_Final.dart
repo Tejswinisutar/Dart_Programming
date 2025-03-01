79ebfb0ef7826065dccbea9c4037f2662449a6d7
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-01 15:29:34 +0530

    final

---
+++ DataType/06_Final.dart
@@ -0,0 +1,9 @@
+void main(){
+   final num=10;
+   print(num);
+   print(num.runtimeType);
+
+  //  final num=20;                     Cannot Reassigned final declared
+  //  print(num);
+  //  print(num.runtimeType);
+}
