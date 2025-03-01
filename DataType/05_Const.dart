a8a856e134be180de7f4106f259bed40270a2424
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-01 15:28:09 +0530

    const

---
+++ DataType/05_Const.dart
@@ -0,0 +1,9 @@
+void main(){
+  const num=20;
+  print(num);
+  print(num.runtimeType);
+
+  // num=10;                              //Cannot Reassigned const declared
+  // print(num);
+  // print(num.runtimeType);
+}
