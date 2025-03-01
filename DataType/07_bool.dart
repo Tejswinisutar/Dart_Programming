ef344cf24ca3144879d2961bf22f6df17f39f77d
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-01 15:29:57 +0530

    bool

---
+++ DataType/07_bool.dart
@@ -0,0 +1,15 @@
+void main(){
+  bool No1 = false;
+  int Num = 8;
+  print(No1);
+  print(No1.runtimeType);
+  if(Num==10)
+  {
+    No1=true;
+  }
+  else
+  {
+     No1=false;
+  }
+  print(No1);
+}
