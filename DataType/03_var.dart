8fb4e8a1d925dcb80ca32a1e756c0eaddf3d6348
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-01 15:27:15 +0530

    var

---
+++ DataType/03_var.dart
@@ -0,0 +1,9 @@
+void main(){
+  var name = 'A';
+  print(name);
+  print(name.runtimeType);
+
+  //name =10 ;       // Cannot Change Data type After first declare
+  print(name);
+  print(name.runtimeType);
+}
