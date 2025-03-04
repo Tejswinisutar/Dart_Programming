506b6ff168bb4cc7227ae27be17b71e8afe9231c
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-04 20:33:38 +0530

    if_else

---
+++ Control_Stetement/02_if_else_Stetement.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main()
+{
+  print('Enter Positive Number');
+  int No = int.parse(stdin.readLineSync()?? "");
+
+  if(No > 0)
+  {
+     print('Positive Number');
+  }
+  else
+  {
+    print('Negative Number');
+  }
+}
