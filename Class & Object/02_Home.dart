6bc4e575356aa64606034278547bd376cf3d8688
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-05 16:46:39 +0530

    Home Deco

---
+++ Class & Object/02_Home.dart
@@ -0,0 +1,14 @@
+
+class Home{
+  dynamic Vita;
+  dynamic Cement;
+}
+
+  void compoundGate(dynamic Vita,dynamic Cement){
+    print('complete Gate & compound');
+  }
+
+  void main(){
+    Home Obj1 = new Home();
+    print(Obj1.Vita);
+ }
