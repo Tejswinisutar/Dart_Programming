40f92188efa87d133e6a241bbbff10b4eec5ebaf
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-09 19:21:47 +0530

    Constructor

---
+++ Constructor/04_Named_Parameter.dart
@@ -0,0 +1,16 @@
+
+class Calc
+ {
+  double Num1;
+  double Num2;
+
+   Calc.Multiplication(this.Num1, this.Num2) 
+   {
+       double multiply = Num1 * Num2;
+       print("Addition: $multiply");
+   }
+}
+void main()
+{
+  Calc.Multiplication(10,20); 
+}
