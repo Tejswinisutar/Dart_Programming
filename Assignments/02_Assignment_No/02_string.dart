cd7646b8b26df0a12c0a5fac1e444e63e6908c51
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-20 20:44:22 +0530

    string

---
+++ Assignments/02_Assignment_No/02_string.dart
@@ -0,0 +1,6 @@
+void main(){                    //Entry Point Fun
+  String name = "Fork";        //Declaration/Initialization 
+  print(name);        
+  print(name.runtimeType);      //RuntimeType
+  print(name.hashCode);         //Virtual Address
+}
