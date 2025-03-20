edaaf43ee56543b288c1e34f5b1ee5b220e21abd
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-20 20:44:45 +0530

    bool

---
+++ Assignments/02_Assignment_No/03_bool.dart
@@ -0,0 +1,7 @@
+void main(){                  //Entry Point Fun
+  bool no1 = true;            //Declaration/Initialization 
+  print(no1);
+  print(no1.runtimeType);      //RuntimeType
+  print(no1.hashCode);          //Virtual Address
+
+}
